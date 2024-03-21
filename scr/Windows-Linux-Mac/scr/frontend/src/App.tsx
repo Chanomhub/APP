d6aae59723792fafab569e1128d08d8b105f9a22
Chanomhub <121768693+Chanomhub@users.noreply.github.com>
2024-03-21 09:15:15 +0700

    name

--- scr/Windows-Linux-Mac/scr/frontend/src/App.tsx
+++ scr/Windows-Linux-Mac/scr/frontend/src/App.tsx
@@ -4,23 +4,20 @@ import './App.css';
 import {Greet} from "../wailsjs/go/main/App";
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e: any) => setName(e.target.value);
-    const updateResultText = (result: string) => setResultText(result);
-
-    function greet() {
-        Greet(name).then(updateResultText);
-    }
 
     return (
         <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
+          <header>
+
+          </header>
+          <body>
+           <div className='Main'>
+            <div>
+                1234
             </div>
+
+           </div>
+          </body>
         </div>
     )
 }
