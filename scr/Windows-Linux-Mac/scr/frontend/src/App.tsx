a838076fdcb409f619476d46c6c73e88632de4b9
Chanomhub <121768693+Chanomhub@users.noreply.github.com>
2024-03-25 03:31:37 +0700

    Update App.tsx

--- scr/Windows-Linux-Mac/scr/frontend/src/App.tsx
+++ scr/Windows-Linux-Mac/scr/frontend/src/App.tsx
@@ -28,24 +28,32 @@ const App: React.FC = () => {
       console.error('Error fetching first page data:', error);
     }
   };
+const getRandomLink = (content: string) => {
+    const parser = new DOMParser();
+    const htmlDoc = parser.parseFromString(content, 'text/html');
+    const links = Array.from(htmlDoc.getElementsByClassName('link'));
+    if (links.length > 0) {
+        const randomIndex = Math.floor(Math.random() * links.length);
+        return links[randomIndex].getAttribute('href') || '';
+    } else {
+        return '#'; // หรือลิงก์เริ่มต้นอื่น ๆ ที่คุณต้องการ
+    }
+};
 
  
 
   const fetchData = async (newPage: number) => {
     try {
-      const response = await fetch('https://api.chanomhub.xyz/fetch-data?page=' + newPage, {
-      
-      });
-
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const pageData = await response.json();
-      setData(pageData);
+        const response = await fetch('https://api.chanomhub.xyz/fetch-data?page=' + newPage);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const pageData = await response.json();
+        setData(pageData);
     } catch (error) {
-      console.error('Error fetching data for page', newPage, ':', error);
+        console.error('Error fetching data for page', newPage, ':', error);
     }
-  };
+};
 
   
 
@@ -88,25 +96,26 @@ const App: React.FC = () => {
       </div>
 
       <div className="faces-container">
-        {data.length > 0 ? (
+    {data.length > 0 ? (
         data.map((item, index) => (
-          <div className="row" key={index}>
-            <div className='block'>
-                <picture>
-                <img src={item.jetpack_featured_media_url} className='images'/>
-                </picture>
+            <div className="row" key={index}>
+                <div className='block'>
+                    <picture>
+                        <a href={getRandomLink(item.content)} target="_blank" rel="noopener noreferrer">
+                            <img src={item.jetpack_featured_media_url} className='images' alt="Featured"/>
+                        </a>
+                    </picture>
+                </div>
+                <div className='title'>
+                    <p>{item.title}</p>
+                </div>
             </div>
-            <div className='title'>
-            <p>{item.title}</p>
-            </div>
-          </div>
         ))
-        ) : (
-            <div>Loading....</div>
-        )}
-      </div>
-      
-    </div>
+    ) : (
+        <div>Loading....</div>
+    )}
+</div>
+
   );
 };
 
